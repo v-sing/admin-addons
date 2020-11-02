f8673ba62bef7a225da2395ec50e4413ccf7701b
此処 <email1946367301@163.com>
2020-11-02 13:31:37 +0800

    体提交

---
+++ src/AddonsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhoulongtao
+ * Date: 2020-11-02
+ * Time: 11:57
+ */
+
+namespace Future\Admin;
+
+
+class AddonsServiceProvider
+{
+    protected $namespace = 'Future\Admin\Controllers';
+
+}
